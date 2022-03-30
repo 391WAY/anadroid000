b4ac04161825759c68e33f54e56ccc9793fc872a
user <10910332@gm.chihlee.edu.tw>
2022-03-30 14:31:44 +0800

    new page finish
    第二頁

--- app/src/main/java/com/example/c10910332/MainActivity.kt
+++ app/src/main/java/com/example/c10910332/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var myt1:TextView=findViewById<TextView>(R.id.dd)
+        var myt1:TextView=findViewById<TextView>(R.id.t1)
         myt1.text = "Hello!!!!"
         var bt1:Button=findViewById(R.id.btn1)
         var bt2:Button=findViewById(R.id.btn2)
---
+++ app/src/main/java/com/example/c10910332/MainActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.c10910332
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
